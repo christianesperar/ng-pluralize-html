7f4b9bdd5698a18139ac1a2e3c49ef26684fb0fc
christianesperar <christianesperar@gmail.com>
2018-01-29 14:00:26 +0800

    Add some enhancement - More code readability and DRY - Use deep watch in case the scope is an object

--- dist/app.js
+++ dist/app.js
@@ -7,7 +7,7 @@
  */
 var ngPluralizeHtml = angular.module('ngPluralizeHtml', []);
 
-ngPluralizeHtml.directive('ngPluralizeHtml', ['$timeout', function($timeout) {
+ngPluralizeHtml.directive('ngPluralizeHtml', ['$timeout', function ($timeout) {
   return {
     restrict: 'AE',
     template:'<span ng-bind-html="htmlPluralize | trustedHtml"></span><span style="display: none"><ng-pluralize ng-if="!offset" count="count" when="when"></ng-pluralize><ng-pluralize ng-if="offset" count="count" offset="offset" when="when"></ng-pluralize></span>',
@@ -18,41 +18,35 @@ ngPluralizeHtml.directive('ngPluralizeHtml', ['$timeout', function($timeout) {
     },
     // Watch all the scope then get the generated string of ng-pluralize then
     // parse it using ng-bind-html
-    link: function($scope, $elem, $attrs) {
+    link: function ($scope, $elem, $attrs) {
       // Get the raw text of ng-pluralize element
       function getHtmlPluralizeText() {
-        $timeout(function() {
+        $timeout(function () {
           $scope.htmlPluralize = $elem.find('ng-pluralize').text();
         }, 100);
       }
 
       // Assign all the parent scope to this scope
-      angular.forEach($scope.$parent, function(value, key) {
-        if (key.indexOf('$') === -1) {
-          $scope.$parent.$watch(key, function(val) {
-            $scope[key] = val;
+      angular.forEach($scope.$parent, function (parentValue, parentScope) {
+        if (parentScope.indexOf('$') === -1) {
+          $scope.$parent.$watch(parentScope, function (newValue) {
+            $scope[parentScope] = newValue;
 
             getHtmlPluralizeText();
-          });
+          }, true);
         }
       });
 
-      $scope.$watch('count', function(val) {
-        getHtmlPluralizeText();
-      });
-
-      $scope.$watch('offset', function(val) {
-        getHtmlPluralizeText();
-      });
-
-      $scope.$watch('when', function(val) {
-        getHtmlPluralizeText();
+      angular.forEach(['count', 'offset', 'when'], function (scope) {
+        $scope.$watch(scope, function () {
+          getHtmlPluralizeText()
+        }, true);
       });
     },
   };
 }]);
 
 // Helper to successfully parse text to html
-ngPluralizeHtml.filter('trustedHtml', ['$sce', function($sce){
+ngPluralizeHtml.filter('trustedHtml', ['$sce', function ($sce){
   return $sce.trustAsHtml;
 }]);
